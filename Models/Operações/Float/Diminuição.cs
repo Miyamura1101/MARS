94e144dc42bdd18bd2231e3ac957aec57ae2a726
Miyamura <felipesilsou1101@gmail.com>
2025-03-22 06:23:51 -0300

    Operacao

--- Models/Operações/Float/Diminuição.cs
+++ Models/Operações/Float/Diminuição.cs
@@ -7,7 +7,7 @@ namespace MARS.Models.Operações.Float
 {
     public class Diminuição_F
     {
-        public static string operacao(float num1, float num2)
+        public static string Operacao(float num1, float num2)
         {
             string url = @"C:/Users/felip/Documents/UFS/4 - Quarto Periodo/OAC/Trabalho de OAC/Operações/Subtração_F.asm";
             return EntradaPadrao.MARS(url, num1, num2);
